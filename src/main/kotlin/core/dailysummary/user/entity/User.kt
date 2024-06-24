f6548ccd9ed2a2ae06c7c46828a9ab5dc9ccda41
kimJaeHyeon <kmr2644@gmail.com>
2024-06-24 17:11:46 +0900

    feat: 유저 엔티티 구현

---
+++ src/main/kotlin/core/dailysummary/user/entity/User.kt
@@ -0,0 +1,28 @@
+package core.dailysummary.user.entity
+
+import core.dailysummary.common.entity.ULIDPrimaryKeyEntity
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.Table
+import java.time.LocalDateTime
+
+@Entity
+@Table(name = "users")
+class User(
+    email: String,
+    createdDate: LocalDateTime,
+    sendTime: Int
+) : ULIDPrimaryKeyEntity() {
+    @Column(unique = true)
+    var email: String = email
+        protected set
+
+    @Column
+    var createdDate: LocalDateTime = createdDate
+        protected set
+
+    @Column
+    var sendTime: Int = sendTime
+        protected set
+
+}
---
+++ src/test/kotlin/core/dailysummary/api/support/MockMvcDsl.kt
@@ -0,0 +1,198 @@
+package core.dailysummary.api.support
+
+import org.springframework.http.HttpMethod
+import org.springframework.restdocs.generate.RestDocumentationGenerator
+import org.springframework.test.web.servlet.*
+
+/**
+ * Create a [ResultActionsDsl] for a GET request. The URL template
+ * will be captured and made available for documentation.
+ * Use this method instead of [MockMvc.get] in order to be able to document the URL template and path parameters
+ * @param urlTemplate a URL template; the resulting URL will be encoded
+ * @param urlVariables zero or more URL variables
+ * @return the DSL allowing to apply expectations, handlers, and to document the request
+ */
+fun MockMvc.docGet(
+    urlTemplate: String,
+    vararg urlVariables: Any?,
+    dsl: MockHttpServletRequestDsl.() -> Unit = {}
+): ResultActionsDsl {
+    return get(urlTemplate, *urlVariables) {
+        requestAttr(RestDocumentationGenerator.ATTRIBUTE_NAME_URL_TEMPLATE, urlTemplate);
+        dsl()
+    }
+}
+
+/**
+ * Create a [ResultActionsDsl] for a POST request. The URL template
+ * will be captured and made available for documentation.
+ * Use this method instead of [MockMvc.post] in order to be able to document the URL template and path parameters
+ * @param urlTemplate a URL template; the resulting URL will be encoded
+ * @param urlVariables zero or more URL variables
+ * @return the DSL allowing to apply expectations, handlers, and to document the request
+ */
+fun MockMvc.docPost(
+    urlTemplate: String,
+    vararg urlVariables: Any?,
+    dsl: MockHttpServletRequestDsl.() -> Unit = {}
+): ResultActionsDsl {
+    return post(urlTemplate, *urlVariables) {
+        requestAttr(RestDocumentationGenerator.ATTRIBUTE_NAME_URL_TEMPLATE, urlTemplate);
+        dsl()
+    }
+}
+
+/**
+ * Create a [ResultActionsDsl] for a PUT request. The URL template
+ * will be captured and made available for documentation.
+ * Use this method instead of [MockMvc.put] in order to be able to document the URL template and path parameters
+ * @param urlTemplate a URL template; the resulting URL will be encoded
+ * @param urlVariables zero or more URL variables
+ * @return the DSL allowing to apply expectations, handlers, and to document the request
+ */
+fun MockMvc.docPut(
+    urlTemplate: String,
+    vararg urlVariables: Any?,
+    dsl: MockHttpServletRequestDsl.() -> Unit = {}
+): ResultActionsDsl {
+    return put(urlTemplate, *urlVariables) {
+        requestAttr(RestDocumentationGenerator.ATTRIBUTE_NAME_URL_TEMPLATE, urlTemplate);
+        dsl()
+    }
+}
+
+/**
+ * Create a [ResultActionsDsl] for a DELETE request. The URL template
+ * will be captured and made available for documentation.
+ * Use this method instead of [MockMvc.delete] in order to be able to document the URL template and path parameters
+ * @param urlTemplate a URL template; the resulting URL will be encoded
+ * @param urlVariables zero or more URL variables
+ * @return the DSL allowing to apply expectations, handlers, and to document the request
+ */
+fun MockMvc.docDelete(
+    urlTemplate: String,
+    vararg urlVariables: Any?,
+    dsl: MockHttpServletRequestDsl.() -> Unit = {}
+): ResultActionsDsl {
+    return delete(urlTemplate, *urlVariables) {
+        requestAttr(RestDocumentationGenerator.ATTRIBUTE_NAME_URL_TEMPLATE, urlTemplate);
+        dsl()
+    }
+}
+
+/**
+ * Create a [ResultActionsDsl] for a HEAD request. The URL template
+ * will be captured and made available for documentation.
+ * Use this method instead of [MockMvc.head] in order to be able to document the URL template and path parameters
+ * @param urlTemplate a URL template; the resulting URL will be encoded
+ * @param urlVariables zero or more URL variables
+ * @return the DSL allowing to apply expectations, handlers, and to document the request
+ */
+fun MockMvc.docHead(
+    urlTemplate: String,
+    vararg urlVariables: Any?,
+    dsl: MockHttpServletRequestDsl.() -> Unit = {}
+): ResultActionsDsl {
+    return head(urlTemplate, *urlVariables) {
+        requestAttr(RestDocumentationGenerator.ATTRIBUTE_NAME_URL_TEMPLATE, urlTemplate);
+        dsl()
+    }
+}
+
+
+/**
+ * Create a [ResultActionsDsl] for an OPTIONS request. The URL template
+ * will be captured and made available for documentation.
+ * Use this method instead of [MockMvc.options] in order to be able to document the URL template and path parameters
+ * @param urlTemplate a URL template; the resulting URL will be encoded
+ * @param urlVariables zero or more URL variables
+ * @return the DSL allowing to apply expectations, handlers, and to document the request
+ */
+fun MockMvc.docOptions(
+    urlTemplate: String,
+    vararg urlVariables: Any?,
+    dsl: MockHttpServletRequestDsl.() -> Unit = {}
+): ResultActionsDsl {
+    return options(urlTemplate, *urlVariables) {
+        requestAttr(RestDocumentationGenerator.ATTRIBUTE_NAME_URL_TEMPLATE, urlTemplate);
+        dsl()
+    }
+}
+
+/**
+ * Create a [ResultActionsDsl] for a PATCH request. The URL template
+ * will be captured and made available for documentation.
+ * Use this method instead of [MockMvc.patch] in order to be able to document the URL template and path parameters
+ * @param urlTemplate a URL template; the resulting URL will be encoded
+ * @param urlVariables zero or more URL variables
+ * @return the DSL allowing to apply expectations, handlers, and to document the request
+ */
+fun MockMvc.docPatch(
+    urlTemplate: String,
+    vararg urlVariables: Any?,
+    dsl: MockHttpServletRequestDsl.() -> Unit = {}
+): ResultActionsDsl {
+    return patch(urlTemplate, *urlVariables) {
+        requestAttr(RestDocumentationGenerator.ATTRIBUTE_NAME_URL_TEMPLATE, urlTemplate);
+        dsl()
+    }
+}
+
+/**
+ * Create a [ResultActionsDsl] for a multipart request. The URL template
+ * will be captured and made available for documentation.
+ * Use this method instead of [MockMvc.multipart] in order to be able to document the URL template and path parameters
+ * @param urlTemplate a URL template; the resulting URL will be encoded
+ * @param urlVariables zero or more URL variables
+ * @return the DSL allowing to apply expectations, handlers, and to document the request
+ */
+fun MockMvc.docMultipart(
+    urlTemplate: String,
+    vararg urlVariables: Any?,
+    dsl: MockHttpServletRequestDsl.() -> Unit = {}
+): ResultActionsDsl {
+    return multipart(urlTemplate, *urlVariables) {
+        requestAttr(RestDocumentationGenerator.ATTRIBUTE_NAME_URL_TEMPLATE, urlTemplate);
+        dsl()
+    }
+}
+
+/**
+ * Create a [ResultActionsDsl] for a request with the given HTTP method. The URL template
+ * will be captured and made available for documentation.
+ * Use this method instead of [MockMvc.multipart] in order to be able to document the URL template and path parameters
+ * @param urlTemplate a URL template; the resulting URL will be encoded
+ * @param urlVariables zero or more URL variables
+ * @return the DSL allowing to apply expectations, handlers, and to document the request
+ */
+fun MockMvc.docRequest(
+    method: HttpMethod,
+    urlTemplate: String,
+    vararg urlVariables: Any?,
+    dsl: MockHttpServletRequestDsl.() -> Unit = {}
+): ResultActionsDsl {
+    return request(method, urlTemplate, *urlVariables) {
+        requestAttr(RestDocumentationGenerator.ATTRIBUTE_NAME_URL_TEMPLATE, urlTemplate);
+        dsl()
+    }
+}
+
+/**
+ * Adds a MockMVC result handler to the chain of handlers applied to the result of a MockMVC request,
+ * which allows documenting the request and its response.
+ *
+ * Example usage:
+ * @sample com.ninjasquad.springrestdocskotlin.mockmvc.andDocumentExample
+ *
+ * @param identifier an identifier for the API call that is being documented
+ * @param configure an extension function taking a [DocumentationScope] as receiver,
+ * and which allows adding and configuring documentation snippets to the documentation
+ * @return the ResultActions, allowing to apply additional result handlers
+ */
+fun ResultActionsDsl.andDocument(
+    identifier: String,
+    dsl: RestDocDsl.() -> Unit = {}
+): ResultActionsDsl {
+    return RestDocDsl().apply(dsl).perform(identifier, this)
+
+}
