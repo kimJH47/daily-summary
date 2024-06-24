507d35157bc67e1bcda93259f6fe6c4d74806fc5
kimJaeHyeon <kmr2644@gmail.com>
2024-06-25 00:15:02 +0900

    fix: request mapping 추가

--- src/main/kotlin/core/dailysummary/api/SubscriptionController.kt
+++ src/main/kotlin/core/dailysummary/api/SubscriptionController.kt
@@ -5,9 +5,11 @@ import core.dailysummary.subscription.dto.SubscriptionRequest
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-@RestController("/api/v1/subscriptions")
+@RestController
+@RequestMapping("/api/v1/subscriptions")
 class SubscriptionController(
     private val subscriptionService: SubscriptionService
 ) {
